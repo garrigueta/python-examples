043c2b58be99cc3c49e23f295f1a923ec5ee0b2a
fgarriga <ferran.garriga@travelport.com>
2021-01-09 18:25:33 +0100

    add rescale example for video media

---
+++ opencv/rescale.py
@@ -0,0 +1,27 @@
+import cv2 as cv
+
+def rescaleFrame(frame, scale=0.5):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions = (width,height)
+
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
+cv.waitKey(0)
+
+#load video
+capture = cv.VideoCapture("C:\git\PyScripts\opencv\\videos\\fog1.mp4")
+
+while   True:
+    isTrue, frame = capture.read()
+
+    frame_resized = rescaleFrame(frame)
+
+    cv.imshow('Video Resized', frame_resized)
+
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+
+capture.release()
+
+cv.waitKey(0)
