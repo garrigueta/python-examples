58410aa24dec5ebfdb494c7284a6148103dd7188
fgarriga <ferran.garriga@travelport.com>
2021-01-10 16:11:05 +0100

    Add more opencv exmples

---
+++ opencv/video.py
@@ -0,0 +1,15 @@
+import cv2 as cv
+
+#load video
+capture = cv.VideoCapture(0)
+
+while   True:
+    isTrue, frame = capture.read()
+
+    cv.imshow('Video', frame)
+
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+capture.release()
+
+cv.waitKey(0)
