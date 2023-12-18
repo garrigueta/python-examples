bb12e7792cdd394f1f185b66293ae72688569f1e
garrigueta <ferran.garriga@gmail.com>
2023-12-18 21:16:46 +0100

    refactor folders

--- MIDI/modulation_shapes.py
+++ MIDI/modulation_shapes.py
@@ -7,7 +7,7 @@ def modulation_shape(signal="sine"):
     t = np.arange(0, 80, 0.1)
 
     if signal == "sine":
-        amplitude = np.sin(np.pi * 2 * t)
+        amplitude = np.cos(t)
     if signal == "triangle":
         amplitude = np.abs((t/np.pi-0.5) % 2-1)*2-1
     if signal == "square":
@@ -15,16 +15,27 @@ def modulation_shape(signal="sine"):
     if signal == "sawtooth":
         amplitude = -((t/np.pi) % 2) + 1
 
-    plt.plot(t[1:60], amplitude[1:60])
+    plt.plot(t[1:80], amplitude[1:80])
     plt.title("Modulation Shape: " + signal)
     plt.xlabel('Time')
     plt.ylabel('Amplitude = sin(time)')
     plt.grid(True, which='both')
     plt.axhline(y=0, color='k')
     plt.show()
+    return amplitude
 
 
-modulation_shape("sine")
-modulation_shape("triangle")
-modulation_shape("square")
-modulation_shape("sawtooth")
+def convert_range(value, in_min, in_max, out_min, out_max):
+    l_span = in_max - in_min
+    r_span = out_max - out_min
+    scaled_value = (value - in_min) / l_span
+    scaled_value = out_min + (scaled_value * r_span)
+    return np.round(scaled_value)
+
+
+amp = modulation_shape("sine")
+
+converted_amplitude = []
+for number in amp:
+    result = convert_range(number, -1.0, 1, 0, 127)
+    converted_amplitude.append(result)
